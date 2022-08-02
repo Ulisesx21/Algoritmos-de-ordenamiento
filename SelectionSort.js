9cd6238ac3d813cf6062006a692f162e5d33ce59
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-08-02 05:45:42 -0300

    -

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,12 +1,12 @@
-function selectionSort(arr){
-  for(let i = 0; i < arr.length; i++){
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
     let min = i;
-    for(let j = i+1; j < arr.length; j++){
-      if(arr[j] < arr[min]) min = j
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j
     }
-    if(min !== i){
-      [arr[i],arr[min]] = [arr[min],arr[i]]
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]]
     }
   }
-	return arr
+  return arr
 }
